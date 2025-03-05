b1d57c462f97914b95ffb447cd92f6dd689902dc
Sergey_Dultsev <dultsev010904@gmail.com>
2025-03-05 15:20:31 +0700

    feat: adding nav sticky

--- src/widgets/navigation/navigation.tsx
+++ src/widgets/navigation/navigation.tsx
@@ -16,36 +16,38 @@ const Navigation: React.FC = () => {
     const currentRoute = usePathname(); 
 
     return (
-        <nav className={styles["nav"]}>
-            <Link href="/" className={`${currentRoute === '/' ? styles["nav-item__active"] :  styles["nav-item"]}`}>
-                <HomeIcon/>
-                <p className={styles["nav-item__descr"]}>Главная</p>
-            </Link>
-            <Link href="/profile" className={styles["nav-item"]}>
-                <ProfileIcon/>
-                <p className={styles["nav-item__descr"]}>Профиль</p>
-            </Link>
-            <Link href="/search" className={styles["nav-item"]}>
-                <SearchIcon/>
-                <p className={styles["nav-item__descr"]}>Поиск</p>
-            </Link>
-            <Link href="/create" className={styles["nav-item"]}>
-                <AddIcon/>
-                <p className={styles["nav-item__descr"]}>Создать</p>
-            </Link>
-            <Link href="/favorite" className={styles["nav-item"]}>
-                <SaveIcon/>
-                <p className={styles["nav-item__descr"]}>Избранное</p>
-            </Link>
-            <Link href="/settings" className={styles["nav-item"]}>
-                <SettingsIcon/>
-                <p className={styles["nav-item__descr"]}>Настройки</p>
-            </Link>
-            <div className={styles["nav-item"]}>
-                <ExitIcon/>
-                <p className={styles["nav-item__descr"]}>Выход</p>
-            </div>
-        </nav>
+        <section className={styles["nav"]}>
+            <nav className={styles["nav-list"]}>
+                <Link href="/" className={`${currentRoute === '/' ? styles["nav-item__active"] :  styles["nav-item"]}`}>
+                    <HomeIcon/>
+                    <p className={styles["nav-item__descr"]}>Главная</p>
+                </Link>
+                <Link href="/profile" className={styles["nav-item"]}>
+                    <ProfileIcon/>
+                    <p className={styles["nav-item__descr"]}>Профиль</p>
+                </Link>
+                <Link href="/search" className={styles["nav-item"]}>
+                    <SearchIcon/>
+                    <p className={styles["nav-item__descr"]}>Поиск</p>
+                </Link>
+                <Link href="/create" className={styles["nav-item"]}>
+                    <AddIcon/>
+                    <p className={styles["nav-item__descr"]}>Создать</p>
+                </Link>
+                <Link href="/favorite" className={styles["nav-item"]}>
+                    <SaveIcon/>
+                    <p className={styles["nav-item__descr"]}>Избранное</p>
+                </Link>
+                <Link href="/settings" className={styles["nav-item"]}>
+                    <SettingsIcon/>
+                    <p className={styles["nav-item__descr"]}>Настройки</p>
+                </Link>
+                <div className={styles["nav-item"]}>
+                    <ExitIcon/>
+                    <p className={styles["nav-item__descr"]}>Выход</p>
+                </div>
+            </nav>
+        </section>
     );
 }
 
