538481b79f562c3f29e0ea1049d76149814c5e2e
Sergey_Dultsev <dultsev010904@gmail.com>
2025-03-05 15:01:05 +0700

    feat: adding state active route

--- src/widgets/navigation/navigation.tsx
+++ src/widgets/navigation/navigation.tsx
@@ -1,4 +1,8 @@
+'use client'
+
 import React from "react";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 import styles from "./navigation.module.scss";
 import HomeIcon from "@/assets/icon/HomeIcon";
 import ProfileIcon from "@/assets/icon/ProfileIcon";
@@ -9,32 +13,34 @@ import SettingsIcon from "@/assets/icon/SettingsIcon";
 import ExitIcon from "@/assets/icon/ExitIcon";
 
 const Navigation: React.FC = () => {
+    const currentRoute = usePathname(); 
+
     return (
         <nav className={styles["nav"]}>
-            <div className={styles["nav-item"]}>
+            <Link href="/" className={`${currentRoute === '/' ? styles["nav-item__active"] :  styles["nav-item"]}`}>
                 <HomeIcon/>
                 <p className={styles["nav-item__descr"]}>Главная</p>
-            </div>
-            <div className={styles["nav-item"]}>
+            </Link>
+            <Link href="/profile" className={styles["nav-item"]}>
                 <ProfileIcon/>
                 <p className={styles["nav-item__descr"]}>Профиль</p>
-            </div>
-            <div className={styles["nav-item"]}>
+            </Link>
+            <Link href="/search" className={styles["nav-item"]}>
                 <SearchIcon/>
                 <p className={styles["nav-item__descr"]}>Поиск</p>
-            </div>
-            <div className={styles["nav-item"]}>
+            </Link>
+            <Link href="/create" className={styles["nav-item"]}>
                 <AddIcon/>
                 <p className={styles["nav-item__descr"]}>Создать</p>
-            </div>
-            <div className={styles["nav-item"]}>
+            </Link>
+            <Link href="/favorite" className={styles["nav-item"]}>
                 <SaveIcon/>
                 <p className={styles["nav-item__descr"]}>Избранное</p>
-            </div>
-            <div className={styles["nav-item"]}>
+            </Link>
+            <Link href="/settings" className={styles["nav-item"]}>
                 <SettingsIcon/>
                 <p className={styles["nav-item__descr"]}>Настройки</p>
-            </div>
+            </Link>
             <div className={styles["nav-item"]}>
                 <ExitIcon/>
                 <p className={styles["nav-item__descr"]}>Выход</p>
