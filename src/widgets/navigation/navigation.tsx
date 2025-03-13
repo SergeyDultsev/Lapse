ab0d829f92ecfd402e74cb5fbfb60776b5485108
SergeyDultsev <121752321+Dultsev2004@users.noreply.github.com>
2025-03-13 15:04:54 +0700

    ref: check route activity in navigation

--- src/widgets/navigation/navigation.tsx
+++ src/widgets/navigation/navigation.tsx
@@ -22,23 +22,23 @@ const Navigation: React.FC = () => {
                     <HomeIcon/>
                     <p className={styles["nav-item__descr"]}>Главная</p>
                 </Link>
-                <Link href="/profile" className={styles["nav-item"]}>
+                <Link href="/profile" className={`${currentRoute === '/profile' ? styles["nav-item__active"] :  styles["nav-item"]}`}>
                     <ProfileIcon/>
                     <p className={styles["nav-item__descr"]}>Профиль</p>
                 </Link>
-                <Link href="/search" className={styles["nav-item"]}>
+                <Link href="/search" className={`${currentRoute === '/search' ? styles["nav-item__active"] :  styles["nav-item"]}`}>
                     <SearchIcon/>
                     <p className={styles["nav-item__descr"]}>Поиск</p>
                 </Link>
-                <Link href="/create" className={styles["nav-item"]}>
+                <Link href="/create" className={`${currentRoute === '/create' ? styles["nav-item__active"] :  styles["nav-item"]}`}>
                     <AddIcon/>
                     <p className={styles["nav-item__descr"]}>Создать</p>
                 </Link>
-                <Link href="/favorite" className={styles["nav-item"]}>
+                <Link href="/favorites" className={`${currentRoute === '/favorites' ? styles["nav-item__active"] :  styles["nav-item"]}`}>
                     <SaveIcon/>
                     <p className={styles["nav-item__descr"]}>Избранное</p>
                 </Link>
-                <Link href="/settings" className={styles["nav-item"]}>
+                <Link href="/settings" className={`${currentRoute === '/settings' ? styles["nav-item__active"] :  styles["nav-item"]}`}>
                     <SettingsIcon/>
                     <p className={styles["nav-item__descr"]}>Настройки</p>
                 </Link>
