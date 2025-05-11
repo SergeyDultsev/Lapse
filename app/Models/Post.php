30946f884bfd23f9ab67f836736709513360350d
SergeyDultsev <121752321+Dultsev2004@users.noreply.github.com>
2025-05-11 14:26:35 +0700

    feat: adding isOpen and isFavorite

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -41,6 +41,34 @@ class Post extends Model implements HasMedia
         return $this->belongsTo(Tier::class, 'tier_id', 'tier_id');
     }
 
+    public function favorite()
+    {
+        return $this->belongsTo(Favorite::class, 'post_id', 'post_id');
+    }
+
+    public function isOpen(?User $user): bool
+    {
+        if($this->tier_id === null) return true;
+        if(!$user) return false;
+
+        $tier = $user->tier();
+
+        foreach ($tier as $tierItem) {
+            if($tierItem->tier && $tierItem->tier->price >= $this->tier->price){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function isFavorite(?User $user): bool
+    {
+        $post = $this->favorite()->first();
+        if(!$post) return false;
+        return false;
+    }
+
     public function getPreviewUrlAttribute(): string|null
     {
         $preview_url = $this->getFirstMedia('posts');
