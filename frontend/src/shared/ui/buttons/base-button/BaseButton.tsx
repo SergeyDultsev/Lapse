7ed74d6fdeebddcf1b3b10d2c983be261c364cf8
SergeyDultsev <dultsev010904@gmail.com>
2025-10-12 16:30:32 +0700

    feat: adding btns in profile

--- frontend/src/shared/ui/buttons/base-button/BaseButton.tsx
+++ frontend/src/shared/ui/buttons/base-button/BaseButton.tsx
@@ -5,13 +5,17 @@ import styles from './BaseButton.module.scss'
 import classNames from "classnames";
 
 interface DefaultButtonProps {
-  className: string,
   description: string,
+  className?: string,
 }
 
 const BaseButton: React.FC<DefaultButtonProps> = ({ description , className}) => {
   return (
-      <button className={classNames(styles['base-button'], className)}>{description}</button>
+      <button
+          className={classNames(styles['base-button'], className)}
+      >
+        {description}
+      </button>
   );
 };
 
