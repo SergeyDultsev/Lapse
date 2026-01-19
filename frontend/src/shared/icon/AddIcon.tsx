14aabe69f3d690a682a8d6cb7369751dd1b06a57
SergeyDultsev <dultsev010904@gmail.com>
2026-01-19 19:49:04 +0700

    fix

--- frontend/src/shared/icon/AddIcon.tsx
+++ frontend/src/shared/icon/AddIcon.tsx
@@ -3,11 +3,11 @@ import React from "react";
 const AddIcon: React.FC = () => {
     return (
         <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <g clip-path="url(#clip0_1414_782)">
+            <g clipPath="url(#clip0_1414_782)">
                 <path d="M20 0H0V20H20V0Z" fill="white" fillOpacity="0.01"/>
                 <path d="M10.0003 18.3337C14.6027 18.3337 18.3337 14.6027 18.3337 10.0003C18.3337 5.39795 14.6027 1.66699 10.0003 1.66699C5.39795 1.66699 1.66699 5.39795 1.66699 10.0003C1.66699 14.6027 5.39795 18.3337 10.0003 18.3337Z" stroke="currentColor" strokeWidth="2" strokeLinejoin="round"/>
-                <path d="M10 6.66699V13.3337" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                <path d="M6.66699 10H13.3337" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M10 6.66699V13.3337" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                <path d="M6.66699 10H13.3337" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
             </g>
         </svg>
     );
