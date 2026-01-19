aff32a84e06d9eb91f20a6985420c66ba30492c3
SergeyDultsev <dultsev010904@gmail.com>
2026-01-19 12:48:38 +0700

    chore: delete .next

--- frontend/.next/dev/server/chunks/ssr/_next-internal_server_app_(main)_topic_sport_page_actions_633ed746.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/(main)/topic/sport/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_%28main%29_topic_sport_page_actions_633ed746.js.map
--- frontend/.next/dev/server/chunks/ssr/_next-internal_server_app_(main)_topic_technologies_page_actions_b8afc144.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/(main)/topic/technologies/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_%28main%29_topic_technologies_page_actions_b8afc144.js.map
