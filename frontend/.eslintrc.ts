9943a062332e2e928ec2ed34ca48e0661d08f098
SergeyDultsev <dultsev010904@gmail.com>
2026-01-30 01:04:54 +0700

    chore: adding eslint + prettier

---
+++ frontend/.eslintrc.ts
@@ -0,0 +1,66 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        node: true,
+        es2024: true,
+    },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        }
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks',
+        'plugin:jsx-a11y/recommended',
+        'plugin:prettier/recommended',
+        'next',
+        'next.core-web-vitals',
+    ],
+    plugins: [
+        '@typescript-eslint',
+        'react',
+        'react-hooks',
+        'jsx-a11y',
+    ],
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    rules: {
+        'prettier/prettier': ['error', {
+            singleQuote: true,
+            semi: true,
+            bracketSpacing: true,
+            trailingComma: 'all',
+        }],
+        'no-unused-vars': 'off',
+        'object-curly-spacing': ['error', 'always'],
+        '@typescript-eslint/no-unused-vars': ['warn', {
+            vars: 'all',
+            args: 'after-used',
+            ignoreRestSiblings: false,
+        }],
+
+        'comma-dangle': ['error', {
+            arrays: 'always-multiline',
+            objects: 'always-multiline',
+            imports: 'always-multiline',
+            exports: 'always-multiline',
+            functions: 'never',
+        }],
+
+        semi: ['error', 'always'],
+        quotes: ['error', 'single'],
+
+        'react/react-in-jsx-scope': 'off',
+        'react/prop-types': 'off',
+    },
+};
