83773625903873aed97f9c39af60a4a8b56e2983
SergeyDultsev <dultsev010904@gmail.com>
2026-02-03 00:14:24 +0700

    fix: docker frontend

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,13 +1,7 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-    webpackDevMiddleware: (config) => {
-        config.watchOptions = {
-            poll: 2000,
-            aggregateTimeout: 300,
-        };
-        return config;
-    },
+    turbopack: {},
 };
 
 export default nextConfig;
