31057e8934aef6bd9277c9db3b58216361df8d29
dultsev <dultsev010904@gmail.com>
2026-07-23 15:19:02 +0700

    feat: add styles for automatic input filling

--- frontend/src/shared/ui/input/input-base/InputBase.tsx
+++ frontend/src/shared/ui/input/input-base/InputBase.tsx
@@ -7,7 +7,7 @@ import classNames from 'classnames';
 interface IInputBase {
     placeholder: string
     typeInput?: 'base' | 'auth' | 'search'
-    type: string
+    type: 'text' | 'email' | 'password'
     className?: string
     required?: boolean
     name?: string
@@ -28,18 +28,20 @@ const InputBase: React.FC<IInputBase> = (
     }
 ) => {
     return (
-        <input
-            className={classNames(
-                styles['input-base'],
-                styles[`input-base__${typeInput}`],
-                className
-            )}
-            type={type}
-            placeholder={placeholder}
-            required={required}
-            value={value}
-            onChange={(e) =>onChange && onChange(name || e.target.value, e.target.value)}
-        />
+        <label>
+            <input
+                className={classNames(
+                    styles['input-base'],
+                    styles[`input-base__${typeInput}`],
+                    className
+                )}
+                type={type}
+                placeholder={placeholder}
+                required={required}
+                value={value}
+                onChange={(e) => onChange && onChange(name || e.target.value, e.target.value)}
+            />
+        </label>
     );
 };
 
